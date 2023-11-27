e242b274f443278cd473941b0b7641e5823434fe
Javon <71572733+jtimmerb@users.noreply.github.com>
2023-11-26 21:44:54 -0500

    Delete Rust/BadUSB directory

--- Rust/BadUSB/target/debug/BadUSB.d
+++
@@ -1,1 +0,0 @@
-/home/javonito/Documents/HSSecurity/proj/Rust/BadUSB/target/debug/BadUSB: /home/javonito/Documents/HSSecurity/proj/Rust/BadUSB/src/main.rs
--- Rust/BadUSB/target/debug/deps/bit_set-59dfb232559fa74b.d
+++
@@ -1,7 +0,0 @@
-/home/javonito/Documents/HSSecurity/proj/Rust/BadUSB/target/debug/deps/libbit_set-59dfb232559fa74b.rmeta: /home/javonito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bit-set-0.2.0/src/lib.rs
-
-/home/javonito/Documents/HSSecurity/proj/Rust/BadUSB/target/debug/deps/libbit_set-59dfb232559fa74b.rlib: /home/javonito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bit-set-0.2.0/src/lib.rs
-
-/home/javonito/Documents/HSSecurity/proj/Rust/BadUSB/target/debug/deps/bit_set-59dfb232559fa74b.d: /home/javonito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bit-set-0.2.0/src/lib.rs
-
-/home/javonito/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bit-set-0.2.0/src/lib.rs:
