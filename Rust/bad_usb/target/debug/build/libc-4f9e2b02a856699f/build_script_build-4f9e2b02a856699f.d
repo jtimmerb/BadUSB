77b3c1680255102e6c3057486f1fbb766065c8f0
Javon <jtimmerb@purdue.edu>
2023-11-29 13:49:38 -0500

    add const for api

---
+++ Rust/bad_usb/target/debug/build/libc-4f9e2b02a856699f/build_script_build-4f9e2b02a856699f.d
@@ -0,0 +1,5 @@
+/Users/echo/BadUSB/Rust/bad_usb/target/debug/build/libc-4f9e2b02a856699f/build_script_build-4f9e2b02a856699f: /Users/echo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.150/build.rs
+
+/Users/echo/BadUSB/Rust/bad_usb/target/debug/build/libc-4f9e2b02a856699f/build_script_build-4f9e2b02a856699f.d: /Users/echo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.150/build.rs
+
+/Users/echo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.150/build.rs:
